e96440c88766b900af76c7419d1acaf0d1392bec
smironescu <sergio.mironescu@marfeel.com>
2021-10-31 21:43:55 +0100

    removed unecessary files

--- backend/src/utils/generators.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateId = void 0;
-function generateId() {
-    return Math.floor(Math.random() * 1000000);
-}
-exports.generateId = generateId;
-//# sourceMappingURL=generators.js.map
